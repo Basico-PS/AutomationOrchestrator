c00c97fd9e57a8d872e518bbc27b6f8e9c3b6724
Mathias Balsløw <49169557+mathiasbalslow@users.noreply.github.com>
2019-10-30 19:07:28 +0100

    Add --noreload to eliminate server CPU usage

--- RUN_SERVER.bat
+++ RUN_SERVER.bat
@@ -1,4 +1,4 @@
 call cd %~dp0
 call venv\scripts\activate
-python automation_orchestrator/manage.py runserver
-REM python automation_orchestrator/manage.py runserver 0.0.0.0:8000
+python automation_orchestrator/manage.py runserver --noreload
+REM python automation_orchestrator/manage.py runserver 0.0.0.0:8000 --noreload

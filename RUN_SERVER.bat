adb08db44845ae5af1635af1f868031f84d30fa7
Mathias Balsløw <49169557+mathiasbalslow@users.noreply.github.com>
2019-10-30 11:02:38 +0100

    Update RUN_SERVER.bat

--- RUN_SERVER.bat
+++ RUN_SERVER.bat
@@ -1,4 +1,4 @@
 call cd %~dp0
 call venv\scripts\activate
-call python automation_orchestrator/manage.py runserver
-REM call python automation_orchestrator/manage.py runserver 0.0.0.0:8000
+python automation_orchestrator/manage.py runserver
+REM python automation_orchestrator/manage.py runserver 0.0.0.0:8000

51d23b5ad832a95e69504382279eff3922aadc31
Mathias Balsløw <mbalslow@basico.dk>
2019-11-04 17:00:39 +0100

    Add Model Field Helptext

--- automation_orchestrator/orchestrator/models.py
+++ automation_orchestrator/orchestrator/models.py
@@ -12,8 +12,8 @@ def get_user_name():
 
 
 class App(models.Model):
-    name = models.CharField(max_length=255)
-    path = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, help_text="Specify the name of the application.")
+    path = models.CharField(max_length=255, help_text="Specify the path to the application. IMPORTANT: Make sure to NOT include quotes.")
 
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_updated = models.DateTimeField(auto_now=True, editable=False)
@@ -23,14 +23,14 @@ class App(models.Model):
 
 
 class Botflow(models.Model):    
-    name = models.CharField(max_length=255)
-    path = models.CharField(max_length=255)
-    queue_if_already_running = models.BooleanField(default=True)
+    name = models.CharField(max_length=255, help_text="Specify the name of the botflow/script/file.")
+    path = models.CharField(max_length=255, help_text="Specify the path to the botflow/script/file. IMPORTANT: Make sure to NOT include quotes.")
+    queue_if_already_running = models.BooleanField(default=True, help_text="Specify whether a new record should be added to the queue when a record with this botflow is already in the queue either 'Pending' or 'Running'.")
     
-    close_bot_automatically = models.BooleanField(default=False)
+    close_bot_automatically = models.BooleanField(default=False, help_text="(Nintex RPA specific setting) Specify whether to automatically close the bot using the /Close /Exit commands.")
     
-    timeout_minutes = models.PositiveIntegerField(default=60, validators=[MinValueValidator(1)])
-    timeout_kill_processes = models.CharField(max_length=255, blank=True)
+    timeout_minutes = models.PositiveIntegerField(default=60, validators=[MinValueValidator(1)], help_text="Specify after how many minutes the botflow should be forcibly killed.")
+    timeout_kill_processes = models.CharField(max_length=255, blank=True, help_text="Specify any additional applications that should be killed in the event of a timeout. To specify multiple, use comma to separate them like this: 'iexplore.exe, explorer.exe'.")
 
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_updated = models.DateTimeField(auto_now=True, editable=False)
@@ -40,21 +40,21 @@ class Botflow(models.Model):
 
 
 class FileTrigger(models.Model):
-    app = models.ForeignKey(App, on_delete=models.CASCADE)
-    botflow = models.ForeignKey(Botflow, on_delete=models.CASCADE)
+    app = models.ForeignKey(App, on_delete=models.CASCADE, help_text="Select the application for this trigger.")
+    botflow = models.ForeignKey(Botflow, on_delete=models.CASCADE, help_text="Select the botflow for this trigger.")
     
-    folder_in = models.CharField(max_length=255)
-    folder_out = models.CharField(max_length=255)
+    folder_in = models.CharField(max_length=255, help_text="Specify the folder for incoming files. When a file is detected in this folder, the trigger will be activated.")
+    folder_out = models.CharField(max_length=255, help_text="Specify the folder for the files to be moved to. When the trigger is activated, the file will be moved to this folder.")
     
-    run_after = models.TimeField(null=True, blank=True)
-    run_until = models.TimeField(null=True, blank=True)
-    run_on_week_days = models.BooleanField(default=True)
-    run_on_weekend_days = models.BooleanField(default=True)
+    run_after = models.TimeField(null=True, blank=True, help_text="Specify a time to limit the trigger to only be active after this time.")
+    run_until = models.TimeField(null=True, blank=True, help_text="Specify a time to limit the trigger to only be active before this time.")
+    run_on_week_days = models.BooleanField(default=True, help_text="Specify whether the trigger should be active on week days.")
+    run_on_weekend_days = models.BooleanField(default=True, help_text="Specify whether the trigger should be active on weekend days.")
     
-    computer_name = models.CharField(max_length=255, default=get_computer_name)
-    user_name = models.CharField(max_length=255, default=get_user_name)
-    priority = models.PositiveIntegerField(default=3, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    activated = models.BooleanField(default=False)
+    computer_name = models.CharField(max_length=255, default=get_computer_name, help_text="Specify on which computer the triggered botflow should run.")
+    user_name = models.CharField(max_length=255, default=get_user_name, help_text="Specify on which user the triggered botflow should run.")
+    priority = models.PositiveIntegerField(default=3, validators=[MinValueValidator(1), MaxValueValidator(5)], help_text="Specify the priority of the trigger. The queued record with the highest priority will always run first.")
+    activated = models.BooleanField(default=False, help_text="Specify the priority of the trigger. The queued record with the highest priority will always run first.")
 
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_updated = models.DateTimeField(auto_now=True, editable=False)

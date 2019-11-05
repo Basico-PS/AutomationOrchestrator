675dc7ccdbdc3af4b7dcd2577878690f505160dc
Mathias Balsløw <mbalslow@basico.dk>
2019-11-06 00:02:32 +0100

    Admin html template prepared for layout customization

--- automation_orchestrator/automation_orchestrator/settings.py
+++ automation_orchestrator/automation_orchestrator/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'automation_orchestrator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

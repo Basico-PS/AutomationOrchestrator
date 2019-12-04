95033d856544d7441fc7bcf6e414f64242bb30f9
Mathias Balsløw <mbalslow@basico.dk>
2019-12-04 13:12:47 +0100

    Improve installation and requirement settings

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,7 +1,7 @@
 call cd %~dp0
 call python -m venv venv
 call venv\scripts\activate
-call pip install -r requirements.txt
+call pip install -r requirements.txt --no-cache-dir
 call python automation_orchestrator/manage.py makemigrations
 call python automation_orchestrator/manage.py migrate
 call python automation_orchestrator/manage.py createsuperuser

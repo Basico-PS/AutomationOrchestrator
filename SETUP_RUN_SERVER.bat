87466129c4978bee15da390ac8b3adaee3c42505
Mathias Balsløw <mbalslow@basico.dk>
2019-12-11 18:38:05 +0100

    Improve the SETUP_RUN_SERVER.bat

--- SETUP_RUN_SERVER.bat
+++ SETUP_RUN_SERVER.bat
@@ -3,10 +3,10 @@
 CALL SET /P server=Would you like to run the server locally or in your network (L/N)?
 
 IF %server% == L (
-	CALL SCHTASKS /CREATE /SC DAILY /TN "AutomationOrchestratorRunServer" /TR %CD%"\run_server\RUN_SERVER_LOCALLY.bat" /ST 00:02 /RI 1 /DU 23:59 /RL HIGHEST /F
+	CALL SCHTASKS /CREATE /SC DAILY /TN "AutomationOrchestratorRunServer" /TR "%CD%\run_server\RUN_SERVER_LOCALLY.bat" /ST 00:02 /RI 1 /DU 23:59 /RL HIGHEST /F
 ) 
 IF %server% == N (
-	CALL SCHTASKS /CREATE /SC DAILY /TN "AutomationOrchestratorRunServer" /TR %CD%"\run_server\RUN_SERVER_NETWORK.bat" /ST 00:02 /RI 1 /DU 23:59 /RL HIGHEST /F
+	CALL SCHTASKS /CREATE /SC DAILY /TN "AutomationOrchestratorRunServer" /TR "%CD%\run_server\RUN_SERVER_NETWORK.bat" /ST 00:02 /RI 1 /DU 23:59 /RL HIGHEST /F
 )
 
 IF NOT %server% == N (

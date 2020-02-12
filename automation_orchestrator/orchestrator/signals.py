e1afefdbbd17ebc7081aed47eb00589129039966
Mathias Balsløw <mbalslow@basico.dk>
2020-02-12 12:45:04 +0100

    Do NOT add the botflow notification sender as a recipient

--- automation_orchestrator/orchestrator/signals.py
+++ automation_orchestrator/orchestrator/signals.py
@@ -15,40 +15,51 @@ def botflow_execution_notification(sender, instance, **kwargs):
     if instance.status == "Pending":
         if "@" in instance.queued_notification:
             email_subject = f"[{instance.pk}] Botflow queued: '{instance.botflow}'"
-            email_to = [email for email in instance.queued_notification.split(",")]
+            email_to = [
+                email for email in instance.queued_notification.split(",")
+            ]
         else:
             return
 
     elif instance.status == "Running":
         if "@" in instance.started_notification:
             email_subject = f"[{instance.pk}] Botflow started: '{instance.botflow}'"
-            email_to = [email for email in instance.started_notification.split(",")]
+            email_to = [
+                email for email in instance.started_notification.split(",")
+            ]
         else:
             return
 
     elif instance.status == "Completed":
         if "@" in instance.completed_notification:
             email_subject = f"[{instance.pk}] Botflow completed: '{instance.botflow}'"
-            email_to = [email for email in instance.completed_notification.split(",")]
+            email_to = [
+                email for email in instance.completed_notification.split(",")
+            ]
         else:
             return
 
     elif "ERROR" in instance.status.upper():
         if "@" in instance.error_notification:
             email_subject = f"[{instance.pk}] Botflow failed: '{instance.botflow}'"
-            email_to = [email for email in instance.error_notification.split(",")]
+            email_to = [
+                email for email in instance.error_notification.split(",")
+            ]
         else:
             return
 
     else:
         if "@" in instance.error_notification:
             email_subject = f"[{instance.pk}] UNKNOWN STATUS"
-            email_to = [email for email in instance.error_notification.split(",")]
+            email_to = [
+                email for email in instance.error_notification.split(",")
+            ]
         else:
             return
 
     try:
-        email_to = [str(email).lower().strip() for email in email_to if "@" in email]
+        email_to = [str(email).lower().strip()
+                    for email in email_to if "@" in email]
         email_from = str(smtp_account.email).lower().strip()
 
         msg = EmailMessage()
@@ -56,7 +67,8 @@ def botflow_execution_notification(sender, instance, **kwargs):
         msg['From'] = email_from
 
         if not email_from in email_to:
-            email_to.append(email_from)
+            # email_to.append(email_from)
+            pass
 
         msg['To'] = ", ".join(email_to)
 
@@ -80,11 +92,14 @@ def botflow_execution_notification(sender, instance, **kwargs):
                 msg['From'] = email_from
 
                 if not email_from in email_to:
-                    email_to.append(email_from)
+                    # email_to.append(email_from)
+                    pass
 
                 msg['To'] = ", ".join(email_to)
 
-                msg.set_content(f"Application: {instance.app}\nBotflow: {instance.botflow}\nTrigger: {instance.trigger}\n\nComputer Name: {instance.computer_name}\nUsername: {instance.user_name}\n\nStatus: {instance.status}\n\nTime Start: {instance.time_start}\nTime End: {instance.time_end}")
+                msg.set_content(
+                    f"Application: {instance.app}\nBotflow: {instance.botflow}\nTrigger: {instance.trigger}\n\nComputer Name: {instance.computer_name}\nUsername: {instance.user_name}\n\nStatus: {instance.status}\n\nTime Start: {instance.time_start}\nTime End: {instance.time_end}"
+                )
 
                 with SMTP_SSL(smtp_account.server, smtp_account.port) as server:
                     server.login(email_from, smtp_account.password)

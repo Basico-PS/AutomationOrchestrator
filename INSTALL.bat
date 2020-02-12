ace3b541c3b846604aaf38c859925116395d6fad
Mathias Balsløw <49169557+mbalslow@users.noreply.github.com>
2020-02-12 14:37:48 +0100

    Make the creation of a super user optional
    This makes upgrades more user-friendly and less confusing

--- INSTALL.bat
+++ INSTALL.bat
@@ -4,3 +4,12 @@ CALL venv\scripts\activate
 CALL pip install -r requirements.txt --no-cache-dir
 CALL python automation_orchestrator/manage.py migrate
 CALL python automation_orchestrator/manage.py createsuperuser
+
+CALL SET /P create_super_user=Would you like to create a super user (Y/N)?
+
+IF %create_super_user% == Y (
+	CALL python automation_orchestrator/manage.py createsuperuser
+)
+
+echo The installation is now complete!
+TIMEOUT 5

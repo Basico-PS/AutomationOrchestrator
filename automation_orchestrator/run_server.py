0cbbf606489df7e064ce167fc9ec3e5a00277f4a
Mathias Balsløw <mbalslow@basico.dk>
2020-05-12 14:03:00 +0200

    Signficant improvements to the run_server logic (no full restart)

--- automation_orchestrator/run_server.py
+++ automation_orchestrator/run_server.py
@@ -15,6 +15,7 @@ INFO_LOG_PATH = "logs\\info_log.txt"
 SERVER_LOG_PATH = "logs\\server_log.txt"
 ERROR_LOG_PATH = "logs\\error_log.txt"
 MANAGE_SERVER_PATH = "automation_orchestrator\\manage.py"
+SHUT_DOWN = False
 
 
 sys.path.append('automation_orchestrator')
@@ -27,10 +28,13 @@ from orchestrator.models import BotflowExecution, PythonFunctionExecution
 
 def database_backup():
     try:
+        db_path = os.path.join(DATABASE_DIR, DATABASE_NAME)
+        db_backup_path = os.path.join(DATABASE_DIR, "backup", datetime.now().strftime("%Y%m%d_") + DATABASE_NAME)
+
         print(f"{datetime.now()}: Performing a backup of the database...")
 
-        db = sqlite3.connect(os.path.join(DATABASE_DIR, DATABASE_NAME))
-        db_backup = sqlite3.connect(os.path.join(DATABASE_DIR, "backup", datetime.now().strftime("%Y%m%d%H%M%S ") + DATABASE_NAME))
+        db = sqlite3.connect(db_path)
+        db_backup = sqlite3.connect(db_backup_path)
 
         with db_backup:
             db.backup(db_backup)
@@ -53,22 +57,20 @@ def database_backup():
 
 
 def main():
+    global SHUT_DOWN
+
     os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-    database_backup()
-
-    start_time_main = datetime.now()
-
-    max_runtime = 10800
     server_runtime = 1800
     sleep_time = 10
     restart_time = 2
     error_count = 0
+    error_count_max = 5
 
     if not os.path.exists(MANAGE_SERVER_PATH):
         print(f"{datetime.now()}: ERROR! The 'manage.py' file was not found, closing down...")
-        sleep(restart_time)
-        return None
+        sleep(sleep_time)
+        sys.exit()
 
     python_exe = 'pythonw.exe'
     if os.path.exists(f'venv\\scripts\\{python_exe}'):
@@ -84,74 +86,68 @@ def main():
         url = f"http://{url}:8000/"
         cmd = [python, MANAGE_SERVER_PATH, 'runserver', '0.0.0.0:8000', '--noreload']
 
-    while True:
-        start_time_loop = datetime.now()
+    start_time_loop = datetime.now()
 
-        with open(SERVER_LOG_PATH, "w") as log_file:
-            with Popen(cmd, stdout=log_file, stderr=log_file) as p:
-                try:
-                    print(f"{datetime.now()}: The server is now running on: {url}")
-                    print(f"{datetime.now()}: The server will automatically restart in {str(server_runtime)} seconds...")
+    database_backup()
 
-                    with open(INFO_LOG_PATH, "w") as file:
-                        file.write(f"Url: {url}\n")
+    with open(SERVER_LOG_PATH, "w") as log_file:
+        with Popen(cmd, stdout=log_file, stderr=log_file) as p:
+            try:
+                print(f"{datetime.now()}: The server is now running on: {url}")
+                print(f"{datetime.now()}: The server will automatically restart in {str(server_runtime / 60)} minutes...")
 
-                    while True:
-                        range(10000)
-                        sleep(sleep_time)
+                with open(INFO_LOG_PATH, "w") as file:
+                    file.write(f"Url: {url}\n")
 
-                        try:
-                            requests.get(url, timeout=restart_time)
-                            server_responded = True
-                        except requests.exceptions.Timeout:
-                            server_responded = False
+                while True:
+                    range(10000)
+                    sleep(sleep_time)
 
-                        if (datetime.now() - start_time_loop).seconds >= server_runtime or os.path.exists(ERROR_LOG_PATH) or not server_responded:
-                            if not BotflowExecution.objects.filter(Q(status="Pending") | Q(status="Running")).exists() and not PythonFunctionExecution.objects.filter(time_end__isnull=True).exists():
-                                if os.path.exists(ERROR_LOG_PATH):
-                                    error_count += 1
+                    try:
+                        requests.get(url, timeout=restart_time)
+                        server_responded = True
+                    except requests.exceptions.Timeout:
+                        server_responded = False
 
-                                    with open(ERROR_LOG_PATH) as error_file:
-                                        error_message = error_file.read()
+                    if (datetime.now() - start_time_loop).seconds >= server_runtime or os.path.exists(ERROR_LOG_PATH) or not server_responded:
+                        if not BotflowExecution.objects.filter(Q(status="Pending") | Q(status="Running")).exists() and not PythonFunctionExecution.objects.filter(time_end__isnull=True).exists():
+                            if os.path.exists(ERROR_LOG_PATH):
+                                error_count += 1
 
-                                    print(f"{datetime.now()}: ERROR OCCURRED: {error_message}")
+                                with open(ERROR_LOG_PATH) as error_file:
+                                    error_message = error_file.read()
 
-                                elif not server_responded:
-                                    print(f"{datetime.now()}: ERROR OCCURRED: The server does not respond...")
+                                print(f"{datetime.now()}: ERROR OCCURRED: {error_message}")
 
-                                print(f"{datetime.now()}: Stopping the server...")
+                            elif not server_responded:
+                                print(f"{datetime.now()}: ERROR OCCURRED: The server does not respond...")
 
-                                p.terminate()
-                                p.wait()
+                            break
 
-                                print(f"{datetime.now()}: Server stopped!")
-                                break
+            except:
+                print(f"{datetime.now()}: Server interrupted!")
+                SHUT_DOWN = True
 
-                            else:
-                                print(f"{datetime.now()}: Waiting to restart as something is either 'Pending' or 'Running'...")
+            else:
+                print(f"{datetime.now()}: Restarting the server!")
+                SHUT_DOWN = False
 
-                except:
-                    print(f"{datetime.now()}: Stopping the server...")
+            finally:
+                print(f"{datetime.now()}: Stopping the server...")
 
-                    p.terminate()
-                    p.wait()
+                p.terminate()
+                p.wait()
 
-                    print(f"{datetime.now()}: Server stopped!")
-                    break
+                sleep(restart_time)
 
-        if error_count >= 3:
-            print(f"{datetime.now()}: NOT restarting the server after {str(error_count)} encountered errors!")
-            sleep(restart_time)
-            break
+                print(f"{datetime.now()}: Server stopped!")
 
-        if (datetime.now() - start_time_main).seconds >= max_runtime:
-            print(f"{datetime.now()}: The server has now been running for more than {str(max_runtime)} seconds and is closing down completely...")
-            sleep(restart_time)
-            break
-
-        print(f"{datetime.now()}: Restarting the server in {str(restart_time)} seconds...")
-        sleep(restart_time)
+                sleep(restart_time)
 
 
 if __name__ == '__main__':
-    main()
+    while 1:
+        main()
+
+        if SHUT_DOWN:
+            break

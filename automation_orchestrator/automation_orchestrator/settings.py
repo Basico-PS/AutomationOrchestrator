9688e300eedd357115c855aae67a975f06aabc89
Mathias Balsløw <49169557+mbalslow@users.noreply.github.com>
2021-11-29 12:27:13 +0100

    Add socket.gethostname() to allowed hosts

--- automation_orchestrator/automation_orchestrator/settings.py
+++ automation_orchestrator/automation_orchestrator/settings.py
@@ -67,9 +67,12 @@ SECRET_KEY = get_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',
-                 'localhost',
-                 socket.gethostbyname_ex(socket.gethostname())[-1][-1]]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    socket.gethostname(),
+    socket.gethostbyname_ex(socket.gethostname())[-1][-1]
+]
 
 # Application definition
 
